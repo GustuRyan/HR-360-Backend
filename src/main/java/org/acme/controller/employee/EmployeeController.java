e8e3f54f97e85a5ef7a44838aab154dc2b64c242
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-02 16:41:51 +0800

    update EmployeeController.java

--- src/main/java/org/acme/controller/employee/EmployeeController.java
+++ src/main/java/org/acme/controller/employee/EmployeeController.java
@@ -2,11 +2,14 @@ package org.acme.controller.employee;
 
 import io.quarkus.security.Authenticated;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import org.acme.controller.auth.LoginController;
 import org.acme.repository.employee.EmployeeEnt;
 import org.acme.repository.employee.EmployeeRepository;
+
+import java.util.Map;
 
 @Path("/api/v1/employee")
 @Authenticated
@@ -19,9 +22,23 @@ public class EmployeeController {
     LoginController loginController;
 
     @GET
+    @Path("/retrieve-detail")
+    public Map<String, Object> retrieveEmployee() {
+        return employeeRepository.retrieveEmployeeDetail(loginController.userProfile().employeeId());
+    }
+
+    @GET
     @Path("/detail")
     public EmployeeEnt getEmployeeDetail() {
         return employeeRepository.getEmployeeDetail(loginController.userProfile().employeeId());
+    }
+
+    @PUT
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateEmployee(@Valid EmployeeEnt employee) {
+        return employeeRepository.updateEmployee(loginController.userProfile().employeeId(), employee);
     }
 
 }
