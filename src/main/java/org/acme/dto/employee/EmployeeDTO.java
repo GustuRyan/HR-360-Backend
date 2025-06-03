e683998099974c3518fba89bed7e693abb1644b5
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-03 21:29:23 +0800

    update EmployeeDTO.java

---
+++ src/main/java/org/acme/dto/employee/EmployeeDTO.java
@@ -0,0 +1,19 @@
+package org.acme.dto.employee;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO {
+    public String fullName;
+    public String address;
+    public String email;
+    public String phone;
+    public Integer gender;
+    public LocalDate birthDate;
+    public String birthPlace;
+    public String bloodType;
+    public Long maritalStatus;
+    public Long nationalityId;
+    public String nameEmg;
+    public String phoneEmg;
+    public String addressEmg;
+}
