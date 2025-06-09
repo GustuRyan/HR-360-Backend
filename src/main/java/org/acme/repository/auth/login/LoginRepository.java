130d3111496e024ac2d24d122c6cf8e3a52fc820
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-06-09 15:31:38 +0800

    update LoginRepository.java

--- src/main/java/org/acme/repository/auth/login/LoginRepository.java
+++ src/main/java/org/acme/repository/auth/login/LoginRepository.java
@@ -3,9 +3,12 @@ package org.acme.repository.auth.login;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 import org.acme.repository.auth.AuthEnt;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.mapper.reflect.ConstructorMapper;
+
+import java.util.Map;
 
 import static org.acme.repository.auth.AuthEnt.*;
 
@@ -22,8 +25,9 @@ public class LoginRepository {
                 .registerRowMapper(ConstructorMapper.factory(AuthEnt.class))
                 .mapTo(AuthEnt.class)
                 .findOne()
-                .orElseThrow(() -> new WebApplicationException("User not found", 404)
-                )
+                .orElseThrow(() -> new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+                        .entity(Map.of("error", "User not found"))
+                        .build()))
         );
     }
 
